75d658b0d0492f8477ffe00c309e93ed1428ed34
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-02-09 19:06:08 +0000

    *** empty log message ***

--- src/main/com/topcoder/shared/messaging/ReceiverController.java
+++ src/main/com/topcoder/shared/messaging/ReceiverController.java
@@ -194,11 +194,13 @@ public class ReceiverController extends Thread {
                     try {
                         log.debug("RECEIVING");
                         msg = (ObjectMessage) qreceiver.receive(blockTime);
+                        log.debug("POSTRECEIVING");
                         //log.debug("got a message " + msg);
                         if (qsession.getTransacted() && autoCommit) {
                         //if (this.transacted && autoCommit)
                             qsession.commit();
                         }
+                        log.debug("POSTPOSTRECEIVING");
                     break;
                     } catch (Exception e) {
                         log.debug("ERROR: Error retreiving next message.");
