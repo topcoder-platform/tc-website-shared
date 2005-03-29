19d0e270d823e5ace0106102bce7412a85a85a30
mktong <mktong@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-03-29 18:07:35 +0000

    PadLeft, PadRight methods added

--- src/main/com/topcoder/shared/util/StringUtil.java
+++ src/main/com/topcoder/shared/util/StringUtil.java
@@ -13,4 +13,37 @@ public final class StringUtil {
         return c + s + c;
     }
 
+    public static String padLeft(String s, int len)
+    {
+    	return padLeft(s, len, ' ');
+    }
+    
+    // Returns the input string padded on the left by characters to fit the given length.
+    public static String padLeft(String s, int len, char c)
+    {
+    	int pad_len = len - s.length();
+    	if (pad_len <= 0) return s;
+    	StringBuffer buf = new StringBuffer();
+    	for (int i=0; i<pad_len; i++)
+    		buf.append(c);
+    	buf.append(s);
+    	return buf.toString();
+    }
+    
+    public static String padRight(String s, int len)
+    {
+    	return padRight(s, len, ' ');
+    }
+    
+    // Returns the input string padded on the right by characters to fit the given length.
+    public static String padRight(String s, int len, char c)
+    {
+    	int pad_len = len - s.length();
+    	if (pad_len <= 0) return s;
+    	StringBuffer buf = new StringBuffer();
+    	buf.append(s);
+    	for (int i=0; i<pad_len; i++)
+    		buf.append(c);
+    	return buf.toString();
+    }
 }
