f40d07c4676de723030c34f47fc34ec1c2acc9af
lbackstrom <lbackstrom@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-08-28 15:43:52 +0000

    merge of long contest

---
+++ shared/trunk/src/main/com/topcoder/shared/common/ServicesConstants.java
@@ -0,0 +1,88 @@
+package com.topcoder.shared.common;
+
+//import com.topcoder.server.common.ApplicationServer;
+
+import com.topcoder.shared.util.SimpleResourceBundle;
+
+/**
+ * ServicesConstants.java
+ *
+ * Created on October 22, 2001
+ */
+
+/**
+ * Contains constant variables for the services package.
+ *
+ * @author Alex Roman
+ * @version 1.0
+ */
+public final class ServicesConstants {
+
+    private ServicesConstants() {
+    }
+
+    private static final SimpleResourceBundle bundle = SimpleResourceBundle.getBundle("Tester");
+
+    /** Run the services in simluation mode...no EJB calls and JMS activity */
+    public static final boolean SIM = false;
+
+    /** base directory of the services package */
+    private static final String BASE = bundle.getString("BASE");
+
+    /** base directory of c++ problem solutions, and boundary checkers */
+    public static final String SOLUTIONS = BASE + "/solutions/";
+
+    /** base directory of java problem user submissions */
+    public static final String JAVA_SUBMISSIONS = BASE + "/submissions/java/";
+    public static final String CSHARP_SUBMISSIONS = BASE + "/submissions/csharp/";
+
+    /** base directory of c++ problem user submissions */
+    public static final String CPP_SUBMISSIONS = BASE + "/submissions/cpp/";
+
+    public static final String LONG_SUBMISSIONS = BASE + "/submissions/long/";
+
+    public static final String APPS_CLASSES = BASE + "/build/classes";
+    public static final String DOTNET_BIN = BASE + "/build/dotNet";
+    public static final String LONG_DOTNET_IO = BASE + "/src/csharp/LongTesterIO.cs";
+    public static final String DOTNET_SECURITY = DOTNET_BIN + "/CSSecurityCheck.exe";
+    public static final String DOTNET_SANDBOX = DOTNET_BIN + "/LongWrapper.exe";
+
+    /** argparser.h C++ header file */
+    public static final String WRAPPER = BASE + "/cpp/wrapper.cc";
+    public static final String LONG_WRAPPER = BASE + "/cpp/long_wrapper.cc";
+    public static final String LONG_IO = BASE + "/cpp/long_io.cc";
+
+    /** The directory path to the C++ sandbox application */
+    public static final String SANDBOX = BASE + "/cpp/sandbox/sandbox";
+    public static final String LONG_SANDBOX = BASE + "/cpp/sandbox/long_sandbox";
+
+    /** The directory path to the timeout application */
+    public static final String ALARM = BASE + "/cpp/timeout/timeout";
+    
+    public static final String RUN = BASE + "/cpp/run.sh";
+
+    /** The package name of the contest solutions directory */
+    //public static String SOLUTIONS_PACKAGE = "cpp.solutions.";
+    public static final String SOLUTIONS_PACKAGE = "com.topcoder.tester.solutions.";
+    //public static String SOLUTIONS_PACKAGE = "solutions.";
+
+    public static final int CONTEST_COMPILE_ACTION = 1;
+    public static final int MPSQAS_COMPILE_ACTION = 2;
+    public static final int LONG_COMPILE_ACTION = 11;
+
+    public static final int USER_TEST_ACTION = 3;
+    public static final int CHALLENGE_TEST_ACTION = 4;
+    public static final int SYSTEM_TEST_ACTION = 5;
+    public static final int PRACTICE_TEST_ACTION = 6;
+    public static final int MPSQAS_TEST_ACTION = 7;
+    public static final int WEB_SERVICE_DEPLOY_ACTION = 8;
+    public static final int AUTO_TEST_ACTION = 9;
+    public static final int LONG_TEST_ACTION = 10;
+    
+    public static final int FAILURE_INCORRECT_RESULT = 1;
+    public static final int FAILURE_TIMEOUT = 2;
+
+    public static final int MAX_LONG_SUBMISSIONS = bundle.getInt("MAX_LONG_SUBMISSIONS");
+    public static final int LONG_SUBMISSION_INTERVAL = bundle.getInt("LONG_SUBMISSION_INTERVAL");
+
+}
