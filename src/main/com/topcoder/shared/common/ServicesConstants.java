60d7bfa210f4cdafdb968c08c7c0e5e8634a2e73
thefaxman <thefaxman@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2005-10-17 19:20:10 +0000

    no message

--- src/main/com/topcoder/shared/common/ServicesConstants.java
+++ src/main/com/topcoder/shared/common/ServicesConstants.java
@@ -46,6 +46,10 @@ public final class ServicesConstants {
     public static final String LONG_DOTNET_IO = BASE + "/src/csharp/LongTesterIO.cs";
     public static final String DOTNET_SECURITY = DOTNET_BIN + "/CSSecurityCheck.exe";
     public static final String DOTNET_SANDBOX = DOTNET_BIN + "/LongWrapper.exe";
+    
+    /** Long contest wrappers */
+    public static final String LONG_CONTEST_MPSQAS_WRAPPER = BASE + "/wrappers/lcontest/LongContestMPSQASWrapper.java";
+    public static final String LONG_CONTEST_USER_WRAPPER = BASE + "/wrappers/lcontest/LongContestUserWrapper.java";
 
     /** argparser.h C++ header file */
     public static final String WRAPPER = BASE + "/cpp/wrapper.cc";
