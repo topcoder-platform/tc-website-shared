482c5e74c9ca301d7ed4f14978005c0a51c5c237
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-07-20 19:29:46 +0000

    add

---
+++ src/main/com/topcoder/shared/language/PythonLanguage.java
@@ -0,0 +1,24 @@
+package com.topcoder.shared.language;
+
+import com.topcoder.shared.problem.DataType;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: Jul 20, 2006
+ */
+public class PythonLanguage extends BaseLanguage {
+
+    public final static int ID = 6;
+    public final static String DESCRIPTION = "Python";
+
+    public final static CSharpLanguage CSHARP_LANGUAGE = new CSharpLanguage();
+
+    public PythonLanguage() {
+        super(ID, DESCRIPTION);
+    }
+
+    public String getMethodSignature(String methodName, DataType returnType, DataType[] paramTypes, String[] paramNames) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
