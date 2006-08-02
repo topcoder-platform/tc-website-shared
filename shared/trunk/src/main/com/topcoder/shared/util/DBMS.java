666afd3d5d1645d9e66757fbff220f1b3d4bce34
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-08-02 18:15:04 +0000

    creative to studio

--- shared/trunk/src/main/com/topcoder/shared/util/DBMS.java
+++ shared/trunk/src/main/com/topcoder/shared/util/DBMS.java
@@ -43,7 +43,7 @@ public class DBMS {
     public final static String TCS_JTS_OLTP_DATASOURCE_NAME = getProperty("TCS_JTS_OLTP_DATASOURCE_NAME", "JTS_TCS_CATALOG");
     public final static String TCS_DW_DATASOURCE_NAME = getProperty("TCS_DW_DATASOURCE_NAME", "TCS_DW");
     public final static String FORUMS_DATASOURCE_NAME = getProperty("FORUMS_DATASOURCE_NAME", "java:JiveDS");
-    public final static String STUDIO_DATASOURCE_NAME = getProperty("CREATIVE_DATASOURCE_NAME", "java:JTS_CREATIVE_OLTP");
+    public final static String STUDIO_DATASOURCE_NAME = getProperty("STUDIO_DATASOURCE_NAME", "java:JTS_STUDIO_OLTP");
 
 
     /**
