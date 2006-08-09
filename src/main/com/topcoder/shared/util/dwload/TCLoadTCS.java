969c5e60e953610d57707f2b1af7909909695283
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-08-09 15:48:50 +0000

    add reliable submission ind to load

--- src/main/com/topcoder/shared/util/dwload/TCLoadTCS.java
+++ src/main/com/topcoder/shared/util/dwload/TCLoadTCS.java
@@ -975,7 +975,7 @@ public class TCLoadTCS extends TCLoad {
                         " and project_id = pr.project_id and cur_version = 1) as review_completed_timestamp, " +
                         "(select count(*) from project_result pr where project_id = p.project_id and pr.passed_review_ind = 1) as num_submissions_passed_review, " +
                         "pr.payment, pr.old_rating, pr.new_rating, " +
-                        "pr.old_reliability, pr.new_reliability, pr.placed, pr.rating_ind, pr.reliability_ind, pr.passed_review_ind, p.project_stat_id, pr.point_adjustment, pr.current_reliability_ind " +
+                        "pr.old_reliability, pr.new_reliability, pr.placed, pr.rating_ind, pr.reliability_ind, pr.passed_review_ind, p.project_stat_id, pr.point_adjustment, pr.current_reliability_ind, pr.reliable_submission_ind " +
                         "from project_result pr, " +
                         "project p, " +
                         "comp_versions cv, " +
@@ -989,12 +989,12 @@ public class TCLoadTCS extends TCLoad {
         final String RESULT_UPDATE =
                 "update project_result set submit_ind = ?, valid_submission_ind = ?, raw_score = ?, final_score = ?, inquire_timestamp = ?, " +
                         "submit_timestamp = ?, review_complete_timestamp = ?, payment = ?, old_rating = ?, new_rating = ?, old_reliability = ?, new_reliability = ?, " +
-                        "placed = ?, rating_ind = ?, reliability_ind = ?, passed_review_ind = ?, points_awarded = ?, final_points = ?, current_reliability_ind=?  where project_id = ? and user_id = ?";
+                        "placed = ?, rating_ind = ?, reliability_ind = ?, passed_review_ind = ?, points_awarded = ?, final_points = ?, current_reliability_ind=?,reliable_submission_ind=?  where project_id = ? and user_id = ?";
 
         final String RESULT_INSERT =
                 "insert into project_result (project_id, user_id, submit_ind, valid_submission_ind, raw_score, final_score, inquire_timestamp," +
                         " submit_timestamp, review_complete_timestamp, payment, old_rating, new_rating, old_reliability, new_reliability, placed, rating_ind, " +
-                        "reliability_ind, passed_review_ind, points_awarded, final_points,current_reliability_ind) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)";
+                        "reliability_ind, passed_review_ind, points_awarded, final_points,current_reliability_ind, reliable_submission_ind) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,?)";
 
         try {
             long start = System.currentTimeMillis();
@@ -1073,8 +1073,9 @@ public class TCLoadTCS extends TCLoad {
                     resultUpdate.setNull(18, Types.DECIMAL);
                 }
                 resultUpdate.setInt(19, projectResults.getInt("current_reliability_ind"));
-                resultUpdate.setLong(20, project_id);
-                resultUpdate.setLong(21, projectResults.getLong("user_id"));
+                resultUpdate.setInt(20, projectResults.getInt("reliable_submission_ind"));
+                resultUpdate.setLong(21, project_id);
+                resultUpdate.setLong(22, projectResults.getLong("user_id"));
 
                 //log.debug("before result update");
                 int retVal = resultUpdate.executeUpdate();
@@ -1113,6 +1114,7 @@ public class TCLoadTCS extends TCLoad {
                         resultInsert.setNull(20, Types.DECIMAL);
                     }
                     resultInsert.setInt(21, projectResults.getInt("current_reliability_ind"));
+                    resultInsert.setInt(22, projectResults.getInt("reliable_submission_ind"));
                     //log.debug("before result insert");
                     resultInsert.executeUpdate();
                     //log.debug("after result insert");
