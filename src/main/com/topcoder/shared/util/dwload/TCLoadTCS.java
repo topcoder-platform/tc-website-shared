ba4afb06172d26b879f817384118c55c70ad4249
pulky <pulky@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-11-01 16:58:28 +0000

    *** empty log message ***

--- src/main/com/topcoder/shared/util/dwload/TCLoadTCS.java
+++ src/main/com/topcoder/shared/util/dwload/TCLoadTCS.java
@@ -150,6 +150,7 @@ public class TCLoadTCS extends TCLoad {
             doLoadContests();
 
             doLoadContestPrize();
+            doLoadUserContestPrize();
 
             doLoadProjects();
 
@@ -1919,6 +1920,84 @@ public class TCLoadTCS extends TCLoad {
 
     public void doLoadContestPrize() throws Exception {
         log.info("load contest prize");
+        PreparedStatement select = null;
+        PreparedStatement insert = null;
+        PreparedStatement update = null;
+        ResultSet rs = null;
+
+        try {
+            long start = System.currentTimeMillis();
+            final String SELECT =
+                    "select cp.contest_prize_id, cp.contest_id, cp.prize_type_id, " + 
+                    "ptl.prize_type_desc, cp.place, cp.prize_amount, cp.prize_desc " +
+                    "from contest_prize cp, prize_type_lu ptl " +
+                    "where cp.prize_type_id = ptl.prize_type_id " +
+                    "and (cp.modify_date > ?) ";
+
+            final String INSERT = "insert into contest_prize (contest_prize_id, contest_id, prize_type_id, " +
+                    "prize_type_desc, place, prize_amount, prize_desc) " +
+                    "values (?, ?, ?, ?, ?, ?, ?) ";
+
+            final String UPDATE = "update contest_prize set contest_id = ?, prize_type_id = ?,  prize_type_desc = ?, " +
+                    "place = ?, prize_amount = ?, prize_desc = ? " +
+                    " where contest_prize_id = ? ";
+
+    
+            //load prizes
+            select = prepareStatement(SELECT, SOURCE_DB);
+            select.setTimestamp(1, fLastLogTime);
+            insert = prepareStatement(INSERT, TARGET_DB);
+            update = prepareStatement(UPDATE, TARGET_DB);
+            rs = select.executeQuery();
+
+            int count = 0;
+            while (rs.next()) {
+                count++;
+                //update record, if 0 rows affected, insert record
+                update.clearParameters();
+                update.setObject(1, rs.getObject("contest_id"));
+                update.setObject(2, rs.getObject("prize_type_id"));
+                update.setObject(3, rs.getObject("prize_type_desc"));
+                update.setObject(4, rs.getObject("place"));
+                update.setObject(5, rs.getObject("prize_amount"));
+                update.setObject(6, rs.getObject("prize_desc"));
+                update.setLong(7, rs.getLong("contest_prize_id"));
+
+                int retVal = update.executeUpdate();
+
+                if (retVal == 0) {
+                    insert.clearParameters();
+                    insert.setLong(1, rs.getLong("contest_prize_id"));
+                    insert.setLong(2, rs.getLong("contest_id"));
+                    insert.setObject(3, rs.getObject("prize_type_id"));
+                    insert.setObject(4, rs.getObject("prize_type_desc"));
+                    insert.setObject(5, rs.getObject("place"));
+                    insert.setObject(6, rs.getObject("prize_amount"));
+                    insert.setObject(7, rs.getObject("prize_desc"));
+
+                    insert.executeUpdate();
+                }
+
+            }
+            log.info("loaded " + count + " records in " + (System.currentTimeMillis() - start) / 1000 + " seconds");
+
+
+        } catch (SQLException sqle) {
+            DBMS.printSqlException(true, sqle);
+            throw new Exception("Load of 'contest_prize' table failed.\n" +
+                    sqle.getMessage());
+
+        } finally {
+            close(rs);
+            close(select);
+            close(insert);
+            close(update);
+        }
+    }
+    
+    
+    public void doLoadUserContestPrize() throws Exception {
+        log.info("load user contest prize");
         PreparedStatement select = null;
         PreparedStatement insert = null;
         PreparedStatement update = null;
