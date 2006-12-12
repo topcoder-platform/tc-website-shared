c565c5540d8c2d84818b8d3ca347f900781c2d21
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-12-12 18:05:51 +0000

    add

---
+++ shared/trunk/src/main/com/topcoder/shared/util/loader/tc/FileBasedRunner.java
@@ -0,0 +1,16 @@
+package com.topcoder.shared.util.loader.tc;
+
+/**
+ * @author dok
+ * @version $Revision$ Date: 2005/01/01 00:00:00
+ *          Create Date: Dec 12, 2006
+ */
+public class FileBasedRunner {
+
+    public static void main(String[] args) {
+        //load up config
+        //call loader
+        
+    }
+
+}
