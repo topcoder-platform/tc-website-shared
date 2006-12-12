957fb062a8f39b62332720c36b58c84278be0fad
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2006-12-12 19:03:16 +0000

    debug

--- src/main/com/topcoder/shared/util/loader/tc/TCLauncher.java
+++ src/main/com/topcoder/shared/util/loader/tc/TCLauncher.java
@@ -155,12 +155,13 @@ public class TCLauncher implements Launcher {
                 }
                 synchronized (lock) {
                     goOn = !lastRetrieverFinished || foundItem;
+                    log.debug("goOn = " + goOn);
                 }
 
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
-
+                    e.printStackTrace();
                 }
             }
         }
