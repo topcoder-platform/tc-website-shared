e5de0080a10588493ea866c20545fb43697373db
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2007-05-02 19:37:50 +0000

    add stuff to clear new cache

--- src/main/com/topcoder/shared/util/dwload/CacheClearer.java
+++ src/main/com/topcoder/shared/util/dwload/CacheClearer.java
@@ -10,6 +10,15 @@
 
 package com.topcoder.shared.util.dwload;
 
+import com.topcoder.shared.util.TCContext;
+import com.topcoder.shared.util.TCResourceBundle;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Set;
+
 /**
  *
  * @author rfairfax
@@ -32,5 +41,55 @@ public class CacheClearer {
             e.printStackTrace();
         }
     }
+
+    public static void removelike(String s) {
+        InitialContext ctx = null;
+        TCResourceBundle b = new TCResourceBundle("cache");
+        try {
+            ctx = TCContext.getInitial(b.getProperty("host_url"));
+            //using reflection so that we don't a lot of nasty dependencies when using the class.
+            Object o = ctx.lookup(b.getProperty("jndi_name"));
+            Method[] methods = o.getClass().getDeclaredMethods();
+            Set keys = null;
+            for (Method m : methods) {
+                if ("getKeys".equals(m.getName())) {
+                    for (Class c : m.getParameterTypes()) {
+                        if ("java.lang.String".equals(c.getName())) {
+                            keys = (Set)m.invoke(o, "/");
+                        }
+                    }
+                }
+            }
+
+            Method myMethod = null;
+            for (Method m : methods) {
+                if ("remove".equals(m.getName())) {
+                    Class[] classes =m.getParameterTypes();
+                    if (classes.length==2 && "java.lang.String".equals(classes[0].getName())
+                            && "java.lang.Object".equals(classes[1].getName())) {
+                        myMethod = m;
+                    }
+                }
+            }
+            String mykey;
+            for (Object key : keys) {
+                if (((String)key).indexOf(s)>=0) {
+                    myMethod.invoke(o, "/", key);
+                }
+            }
+
+
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } finally {
+            TCContext.close(ctx);
+        }
+
+    }
+
     
 }
