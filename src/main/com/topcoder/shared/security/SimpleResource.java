91a885a5ede64552d55accb2246e598ff63628d9
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-02-15 21:21:30 +0000

    add equals method

--- src/main/com/topcoder/shared/security/SimpleResource.java
+++ src/main/com/topcoder/shared/security/SimpleResource.java
@@ -3,7 +3,8 @@ package com.topcoder.shared.security;
 /**
  * Minimal implementation of Resource.
  *
- * @author Ambrose Feinstein
+ * @author Ambrose Feinstein,dok
+ * @version $Revision$ $Date$
  */
 public class SimpleResource implements Resource {
 
@@ -15,5 +16,20 @@ public class SimpleResource implements Resource {
 
     public String getName() {
         return name;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SimpleResource that = (SimpleResource) o;
+
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+
+        return true;
+    }
+
+    public int hashCode() {
+        return (name != null ? name.hashCode() : 0);
     }
 }
