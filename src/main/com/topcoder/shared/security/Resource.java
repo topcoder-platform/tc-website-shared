a91d9204425c62456a0b96697fdf71bd25b67c6c
gpaul <gpaul@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2008-02-28 19:48:47 +0000

    make serializable

--- src/main/com/topcoder/shared/security/Resource.java
+++ src/main/com/topcoder/shared/security/Resource.java
@@ -1,12 +1,15 @@
 package com.topcoder.shared.security;
 
+import org.jboss.cache.aop.annotation.Serializable;
+
 /**
  * interface for resources, they could be web pages, web applications
  * etc.  anything that one might require authentication for access to.
  *
- * @author Greg Paul
+ * @author dok
+ * @version $Id$
  */
-public interface Resource {
+public interface Resource extends Serializable {
 
     public String getName();
 }
